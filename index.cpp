7de5367af83f167d499f37998dd367b4bf941712
Aurelie Gedeon <agedeon540@gmail.com>
2022-04-05 14:59:53 -0400

    first commit; messing with C++

---
+++ index.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void showMenu()
+{
+    cout << "**********MENU**********" << endl;
+    cout << "1. Check balance" << endl;
+    cout << "2. Deposit" << endl;
+    cout << "3. Withdraw" << endl;
+    cout << "4. Exit" << endl;
+    cout << "*************************" << endl;
+}
+
+int main()
+{
+    showMenu();
+}
